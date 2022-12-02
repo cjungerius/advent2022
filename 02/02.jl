56afd59bc5c62af312b0d04f73ab6701d3ed0618
Chris Jungerius <cjungerius@gmail.com>
2022-12-02 08:51:41 +0100

    matrices 4 lyfe

--- 02/02.jl
+++ 02/02.jl
@@ -7,31 +7,29 @@ function scorerps(input)
 
     #sorted by you playing R P or S:
     partonemat = [
-        [1 + 3, 2 + 6, 3 + 0],           #outcomes for opponent playing A/rock
-        [1 + 0, 2 + 3, 3 + 6],           #outcomes for opponent playing B/paper
-        [1 + 6, 2 + 0, 3 + 3]            #outcomes for opponent playing C/scissors
+        1+3 2+6 3+0;           #outcomes for opponent playing A/rock
+        1+0 2+3 3+6;           #outcomes for opponent playing B/paper
+        1+6 2+0 3+3           #outcomes for opponent playing C/scissors
     ]
 
     #sorted by W D L:
     parttwomat = [
-        [0 + 3, 3 + 1, 6 + 2],
-        [0 + 1, 3 + 2, 6 + 3],
-        [0 + 2, 3 + 3, 6 + 1]
+        0+3 3+1 6+2;
+        0+1 3+2 6+3;
+        0+2 3+3 6+1
     ]
 
     oppdict = Dict(["A", "B", "C"] .=> [1, 2, 3])
     playerdict = Dict(["X", "Y", "Z"] .=> [1, 2, 3])
 
-    oppidx = [oppdict[k] for k in input[:, 1]]
-    partonescores = [partonemat[o] for o in oppidx]
-    parttwoscores = [parttwomat[o] for o in oppidx]
-
     partone = 0
     parttwo = 0
 
-    for (i, m) in enumerate(input[:, 2])
-        partone += partonescores[i][playerdict[m]]
-        parttwo += parttwoscores[i][playerdict[m]]
+    for i in 1:size(input)[1]
+        x = oppdict[input[i,1]]
+        y = playerdict[input[i,2]]
+        partone += partonemat[x,y]
+        parttwo += parttwomat[x,y]
     end
 
     (partone, parttwo)
