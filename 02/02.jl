2e61c4f0471c6d56930d641d49f8e96984334ace
Chris Jungerius <cjungerius@gmail.com>
2022-12-02 08:54:08 +0100

    slight formatting, call function in file

--- 02/02.jl
+++ 02/02.jl
@@ -7,15 +7,15 @@ function scorerps(input)
 
     #sorted by you playing R P or S:
     partonemat = [
-        1+3 2+6 3+0;           #outcomes for opponent playing A/rock
-        1+0 2+3 3+6;           #outcomes for opponent playing B/paper
+        1+3 2+6 3+0           #outcomes for opponent playing A/rock
+        1+0 2+3 3+6           #outcomes for opponent playing B/paper
         1+6 2+0 3+3           #outcomes for opponent playing C/scissors
     ]
 
     #sorted by W D L:
     parttwomat = [
-        0+3 3+1 6+2;
-        0+1 3+2 6+3;
+        0+3 3+1 6+2
+        0+1 3+2 6+3
         0+2 3+3 6+1
     ]
 
@@ -26,11 +26,13 @@ function scorerps(input)
     parttwo = 0
 
     for i in 1:size(input)[1]
-        x = oppdict[input[i,1]]
-        y = playerdict[input[i,2]]
-        partone += partonemat[x,y]
-        parttwo += parttwomat[x,y]
+        x = oppdict[input[i, 1]]
+        y = playerdict[input[i, 2]]
+        partone += partonemat[x, y]
+        parttwo += parttwomat[x, y]
     end
 
     (partone, parttwo)
 end
+
+partone, parttwo = scorerps(input)
