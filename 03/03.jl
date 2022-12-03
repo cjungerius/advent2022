22f3f00da345a8548d8b0339825b009913b433da
Chris Jungerius <cjungerius@gmail.com>
2022-12-03 13:40:55 +0100

    improve formatting, use sets for speedup

--- 03/03.jl
+++ 03/03.jl
@@ -1,26 +1,25 @@
 input = readlines("input.txt")
 
 function partone(input)
-    priorities = Dict(['a':'z'...,'A':'Z'...] .=> [1:52...])
+    priorities = Dict(['a':'z'..., 'A':'Z'...] .=> [1:52...])
     prioritysum = 0
     for line in input
         l = length(line)
-        commonitem = intersect(line[1:l÷2], line[l÷2+1:end])[1]
-        prioritysum += priorities[commonitem]
+        commonitem = Set(line[1:l÷2]) ∩ Set(line[l÷2+1:end])
+        prioritysum += priorities[pop!(commonitem)]
     end
     prioritysum
 end
 
 function parttwo(input)
-    priorities = Dict(['a':'z'...,'A':'Z'...] .=> [1:52...])
+    priorities = Dict(['a':'z'..., 'A':'Z'...] .=> [1:52...])
     prioritysum = 0
 
-    input = reshape(input,(3,:))
+    input = reshape(input, (3, :))
 
     for i in 1:size(input)[2]
-        commonitem = intersect(input[1,i], input[2,i], input[3,i])[1]
-        prioritysum += priorities[commonitem]
+        commonitem = Set(input[1, i]) ∩ Set(input[2, i]) ∩ Set(input[3, i])
+        prioritysum += priorities[pop!(commonitem)]
     end
-
     prioritysum
 end
