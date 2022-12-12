2388af4c43ce499b589edf9db52a217c37449480
Chris Jungerius <37599089+cjungerius@users.noreply.github.com>
2022-12-12 15:57:08 +0100

    Solve day 12
    commit ee91bb840f53dbcf0545e23c0be861c112489a85
    Author: Chris Jungerius <37599089+cjungerius@users.noreply.github.com>
    Date:   Mon Dec 12 15:55:33 2022 +0100
    
        refactor day 12
    
    commit de6326bae6df29ee9e193bf4f7d4870701c32dfa
    Author: cjungerius <cjungerius@users.noreply.github.com>
    Date:   Mon Dec 12 10:16:30 2022 +0000
    
        Update badges
    
    commit f65b5d2fe269b86e6277f0076813aaa2b1d805c6
    Author: Chris Jungerius <cjungerius@gmail.com>
    Date:   Mon Dec 12 11:16:00 2022 +0100
    
        initial solution

---
+++ src/12.jl
@@ -0,0 +1,77 @@
+module Day12
+
+using DataStructures
+
+function preprocess(io::IO)
+
+    input = []
+
+
+    for line in eachline(io)
+        push!(input,[c for c in line])
+    end
+    
+    input = vcat(permutedims.(input)...)
+end
+
+function neighbours(input,idx)
+    y = idx[1]
+    x = idx[2]
+    candidates = [(y-1,x),(y+1,x),(y,x-1),(y,x+1)]
+    neighbourlist = []	
+    for (a,b) in candidates
+        if 0 < a <= size(input)[1] && 0 < b <= size(input)[2]
+            push!(neighbourlist,CartesianIndex(a,b))
+        end
+    end
+    neighbourlist
+end
+
+function bfs(io::IO)
+
+    input = preprocess(io)
+    distance = fill(Inf,size(input))
+    visited = fill(false,size(input))
+
+    elevationDict = Dict(['a':'z'...] .=> [1:26...])
+    elevationDict['S'] = 1
+    elevationDict['E'] = 26
+    
+    # we go backwards because that makes part two easier
+    start = findfirst(x->x=='E', input)
+    goalone = findfirst(x->x=='S', input)
+    goaltwo = findall(x->x=='a', input)
+
+    distance[start] = 0
+    visited[start] = true
+    q = Queue{CartesianIndex}()
+    enqueue!(q,start)
+
+    #populate the whole map: no early stopping so we can do partone and parttwo simultaneously
+    while !isempty(q)
+        u = dequeue!(q)
+        for n in neighbours(input,u)
+            a = elevationDict[input[u]]
+            b = elevationDict[input[n]]
+            # this is the only thing you have to change to go backwards: we invert the rule about elevation
+            b < (a-1) && continue
+            dist = distance[u]+1
+            distance[n] = min(dist,distance[n])
+            if !visited[n]
+                visited[n] = true
+                enqueue!(q,n)
+            end
+        end
+    end
+
+    partone = distance[goalone]
+    parttwo = minimum([distance[coord] for coord in goaltwo])
+
+    partone, parttwo
+end
+
+function solutions(io::String="data/12.txt")
+    partone, parttwo = bfs(open(io))
+end
+
+end
