b858f85854993d8a986ed6809187fea364386e30
Chris Jungerius <cjungerius@gmail.com>
2022-12-16 10:46:18 +0100

    Solve day 15
    commit dacbd50505122d0e68242bf349289c61be881041
    Author: Chris Jungerius <cjungerius@gmail.com>
    Date:   Thu Dec 15 15:59:26 2022 +0100
    
        refactor, add alternative border walking option
    
    commit c3cf04a5a0562cc7568441449f16d32a46afa982
    Merge: 6cc544b 3bc9bf7
    Author: Chris Jungerius <cjungerius@gmail.com>
    Date:   Thu Dec 15 14:51:54 2022 +0100
    
        Merge branch 'day15' of https://github.com/cjungerius/Advent2022.jl into day15
    
    commit 6cc544b4a54a64c044866a3888a4a39dbd0f5e0c
    Author: Chris Jungerius <cjungerius@gmail.com>
    Date:   Thu Dec 15 14:51:50 2022 +0100
    
        sensor class ftw
    
    commit 74a7c03275c779b78826746ddcb137e6dafd4841
    Author: Chris Jungerius <cjungerius@gmail.com>
    Date:   Thu Dec 15 13:09:06 2022 +0100
    
        refactor (~24 seconds to run)
    
    commit 3bc9bf772917ee000d9c82638d767234609960fb
    Author: cjungerius <cjungerius@users.noreply.github.com>
    Date:   Thu Dec 15 10:43:19 2022 +0000
    
        Update badges
    
    commit a43194ee49ed14d7209d808300861119681b83b2
    Author: Chris Jungerius <cjungerius@gmail.com>
    Date:   Thu Dec 15 11:42:47 2022 +0100
    
        initial (horrendous) solution to day 15

---
+++ src/15.jl
@@ -0,0 +1,110 @@
+module Day15
+
+#part 1 steps:
+#got set of ranges
+#noticed they all overlap
+#distance of minimum to maximum *-1* (because 1 beacon present in line) = answer
+
+struct Sensor
+    x::Int
+    y::Int
+    mdist::Int
+end
+
+function createmap(io = "data/15.txt")
+        nummatch = r"(-?[0-9]+)"
+        sensors = []
+        beacons = []
+        for line in eachline(io)
+            items = [parse(Int, x.captures[1]) for x in eachmatch(nummatch, line)]
+            push!(sensors, (items[1:2]))
+            push!(beacons, (items[3:4]))
+        end
+    sensors, beacons
+end
+
+function createsensors(sensors,beacons)
+    sensorarray = Sensor[]
+    for (sensor, beacon) in zip(sensors, beacons)
+        manhattan = abs(sensor[1]-beacon[1]) + abs(sensor[2]-beacon[2])
+        push!(sensorarray,Sensor(sensor[1],sensor[2],manhattan))
+    end
+    sensorarray
+end
+
+
+function sensorrange(sensors,y)
+
+    coverage = Array[]
+
+    for sensor in sensors
+        diff = sensor.mdist - abs(sensor.y-y)
+        diff ≥ 0 && push!(coverage, [sensor.x-diff,sensor.x+diff])
+    end
+    sort!(coverage)
+end
+
+function checkgap(coverage,rmin,rmax)::Union{Nothing, Int}
+
+    e = 0
+    for (a,b) in coverage
+        if b < rmin 
+            continue
+        elseif a > rmax 
+            return nothing
+        elseif a > e+1
+            return a-1
+        end
+        e = max(e,b)
+    end
+    nothing
+end
+
+function coveragelength(coverage)
+
+    xmin = minimum(x[1] for x in coverage)
+    xmax = maximum(x[2] for x in coverage)
+    if isnothing(checkgap(coverage,xmin,xmax))
+        return length(xmin:xmax)
+    end
+end
+
+function findspot(sensors, rmin=0,rmax=4000000)
+
+    for y in rmin:rmax
+       c = sensorrange(sensors,y)
+       x = checkgap(c,rmin,rmax)
+       !isnothing(x) && return (x,y)
+    end
+end
+
+function getborder(sensor::Sensor)
+    edge = []
+    bdist = sensor.mdist+1
+    for dx in 0:bdist
+        dy = bdist - dx
+        push!(edge,(sensor.x+dx,sensor.y+dy))
+        push!(edge,(sensor.x-dx,sensor.y+dy))
+        push!(edge,(sensor.x+dx,sensor.y-dy))
+        push!(edge,(sensor.x-dx,sensor.y-dy))
+    end
+    edge
+end
+
+function walkborders(sensors,rmin, rmax)
+
+    for (i,sensor) in enumerate(sensors)
+        border = getborder(sensor)
+        for b in border
+            rmin < b[1] < rmax || continue
+            rmin < b[2] < rmax || continue
+            any([inrange(s,b) for s in sensors]) || return b
+        end
+    end
+end
+
+function inrange(sensor,(x,y))
+    abs(sensor.x - x) + abs(sensor.y - y) ≤ sensor.mdist
+end
+
+end
