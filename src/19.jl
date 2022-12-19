ce0994abc5ac54505b2f0b7aeb30329ebcef7938
Chris Jungerius <cjungerius@gmail.com>
2022-12-19 20:18:17 +0100

    Solve day 19
    commit 27bf62ad41e583f96758e07d2c92e115ec9166c8
    Merge: d48e80b 4a149b3
    Author: Chris Jungerius <37599089+cjungerius@users.noreply.github.com>
    Date:   Mon Dec 19 18:42:20 2022 +0100
    
        Merge branch 'day19' of https://github.com/cjungerius/Advent2022.jl into day19
    
    commit d48e80b94d4e929d1a231dcbd58c50869e147bba
    Author: Chris Jungerius <37599089+cjungerius@users.noreply.github.com>
    Date:   Mon Dec 19 18:41:40 2022 +0100
    
        solve pt2 with an MC approach because lmao
    
    commit 4a149b3ee6b807b13a4e2a8bd72245bacab35f35
    Author: cjungerius <cjungerius@users.noreply.github.com>
    Date:   Mon Dec 19 16:42:49 2022 +0000
    
        Update badges
    
    commit 6c7b2cc22a29971bb71c1d4b6a563235b26207ce
    Author: Chris Jungerius <37599089+cjungerius@users.noreply.github.com>
    Date:   Mon Dec 19 17:42:21 2022 +0100
    
        solve part 1

---
+++ src/19.jl
@@ -0,0 +1,168 @@
+module Day19
+
+using DataStructures
+using Memoize
+
+struct State
+        time::Int
+        ore::Int
+        clay::Int
+        obsidian::Int
+        geodes::Int
+        d_ore::Int
+        d_clay::Int
+        d_obsidian::Int
+        d_geodes::Int
+end
+
+function f(io="data/19.txt")
+        blueprints = Array[]
+        for line in eachline(io)
+                ms = eachmatch(r"(\d+)", line)
+                push!(blueprints, [parse(Int, m[1]) for m in ms][2:end])
+        end
+
+        blueprints
+end
+
+function testblueprint(bp)
+
+        orecost = bp[1]
+        claycost = bp[2]
+        obscost = bp[3:4]
+        geocost = bp[5:6]
+
+        start = State(0, 0, 0, 0, 0, 1, 0, 0, 0)
+
+        mem = Dict()
+        
+        function dfs(state::State)
+
+                if get(mem,state,-1) ≥ 0
+                        return mem[state]
+                end
+
+                time = state.time + 1
+                ore = state.ore + state.d_ore
+                clay = state.clay + state.d_clay
+                obsidian = state.obsidian + state.d_obsidian
+                geodes = state.geodes + state.d_geodes
+                d_ore = state.d_ore
+                d_clay = state.d_clay
+                d_obsidian = state.d_obsidian
+                d_geodes = state.d_geodes           
+
+                if time == 24
+                        return geodes
+                end
+
+                outcomes = Int[]
+
+                if state.ore ≥ geocost[1] && state.obsidian ≥ geocost[2] #always build a geo bot if we can
+                        push!(outcomes, dfs(State(time, ore - geocost[1], clay, obsidian - geocost[2], geodes, d_ore, d_clay, d_obsidian, d_geodes + 1)))
+                elseif state.ore ≥ obscost[1] && state.clay ≥ obscost[2] && d_obsidian < geocost[2] #only increase obsidian production if it makes sense
+                        push!(outcomes, dfs(State(time, ore - obscost[1], clay - obscost[2], obsidian, geodes, d_ore, d_clay, d_obsidian + 1, d_geodes)))
+
+                        if state.ore ≥ claycost && d_clay < obscost[2]
+                                push!(outcomes, dfs(State(time, ore - claycost, clay, obsidian, geodes, d_ore, d_clay + 1, d_obsidian, d_geodes)))
+                        end
+
+                        if state.ore ≥ orecost && d_ore < max(orecost,claycost,obscost[1],geocost[1])
+                                push!(outcomes, dfs(State(time, ore - orecost, clay, obsidian, geodes, d_ore + 1, d_clay, d_obsidian, d_geodes)))
+                        end
+
+                        push!(outcomes, dfs(State(time, ore, clay, obsidian, geodes, d_ore, d_clay, d_obsidian, d_geodes)))
+
+                elseif state.ore ≥ claycost && d_clay < obscost[2] #same for clay
+                        push!(outcomes, dfs(State(time, ore - claycost, clay, obsidian, geodes, d_ore, d_clay + 1, d_obsidian, d_geodes)))
+
+                        if state.ore ≥ orecost && d_ore < max(orecost,claycost,obscost[1],geocost[1])
+                                push!(outcomes, dfs(State(time, ore - orecost, clay, obsidian, geodes, d_ore + 1, d_clay, d_obsidian, d_geodes)))
+                        end
+
+                        push!(outcomes, dfs(State(time, ore, clay, obsidian, geodes, d_ore, d_clay, d_obsidian, d_geodes)))
+
+                elseif state.ore ≥ orecost && d_ore < max(orecost,claycost,obscost[1],geocost[1]) #and same for ore
+                        push!(outcomes, dfs(State(time, ore - orecost, clay, obsidian, geodes, d_ore + 1, d_clay, d_obsidian, d_geodes)))
+                        push!(outcomes, dfs(State(time, ore, clay, obsidian, geodes, d_ore, d_clay, d_obsidian, d_geodes)))
+                else
+                        push!(outcomes, dfs(State(time, ore, clay, obsidian, geodes, d_ore, d_clay, d_obsidian, d_geodes)))
+                end
+
+                
+                outcome = maximum(outcomes)
+
+                mem[state] = outcome
+
+                return outcome
+        end
+
+        
+        maxscore = dfs(start)
+end
+
+function testblueprint_mc(bp,n=100000)
+
+        orecost = bp[1]
+        claycost = bp[2]
+        obscost = bp[3:4]
+        geocost = bp[5:6]
+
+        maxscore = 0
+
+        
+        for i in 1:n
+                
+                ore = 0
+                clay = 0
+                obsidian = 0
+                geodes = 0
+                d_ore = 1
+                d_clay = 0
+                d_obsidian = 0
+                d_geodes = 0
+
+                for t in 1:32
+
+                        canbuildgeo = ore ≥ geocost[1] && obsidian ≥ geocost[2]
+
+                        canbuildore = ore ≥ orecost && d_ore < max(claycost,obscost[1],geocost[1])
+                        canbuildclay = ore ≥ claycost && d_clay < obscost[2]
+                        canbuildobsidian = ore ≥ obscost[1] && clay ≥ obscost[2] && d_obsidian < geocost[2]
+
+                        ore += d_ore
+                        clay += d_clay
+                        obsidian += d_obsidian
+                        geodes += d_geodes
+
+                        t==32 && continue
+
+                        if canbuildgeo
+                                d_geodes += 1
+                                ore -= geocost[1]
+                                obsidian -= geocost[2]
+                        elseif any([canbuildore, canbuildclay,canbuildobsidian])
+                                options = [canbuildore, canbuildclay, canbuildobsidian,true]
+                                step = argmax(rand(4).*options)
+                                if step == 1
+                                        d_ore += 1
+                                        ore -= orecost
+                                elseif step == 2
+                                        d_clay += 1
+                                        ore -= claycost
+                                elseif step == 3
+                                        d_obsidian += 1
+                                        ore -= obscost[1]
+                                        clay -= obscost[2]
+                                end
+                        end
+
+                end
+                maxscore = max(maxscore,geodes)
+        end
+
+
+        maxscore
+end
+
+end
