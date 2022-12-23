aee89267057b265eea176226d9c2e936e1df5d44
Chris Jungerius <cjungerius@gmail.com>
2022-12-23 23:09:39 +0100

    Solve day 23
    commit 41efa3acf69504256d42a7db431483dfa5865feb
    Author: Chris Jungerius <cjungerius@gmail.com>
    Date:   Fri Dec 23 23:09:24 2022 +0100
    
        add solution function
    
    commit 3d80254a73c3647431d815769678e9b4d68433a3
    Author: Chris Jungerius <37599089+cjungerius@users.noreply.github.com>
    Date:   Fri Dec 23 20:53:35 2022 +0100
    
        optimize pt2 for speed (15sec -> <1sec)
    
    commit 130c8a6433729675bd7a096f258c2adb73e5542a
    Author: Chris Jungerius <37599089+cjungerius@users.noreply.github.com>
    Date:   Fri Dec 23 20:33:08 2022 +0100
    
        solve part 2
    
    commit 89523e58c37c84e677962332f32a1320549d3dfa
    Author: Chris Jungerius <37599089+cjungerius@users.noreply.github.com>
    Date:   Fri Dec 23 19:22:41 2022 +0100
    
        begin solve pt1

---
+++ src/23.jl
@@ -0,0 +1,88 @@
+module Day23
+
+using StatsBase
+
+function make_elves(io)::Vector{Tuple{Int,Int}}
+        elves = Tuple{Int,Int}[]
+        for (y, line) in enumerate(eachline(io))
+                xs = findall(==('#'), line)
+                push!(elves, [(x, y) for x in xs]...)
+        end
+
+        elves
+end
+
+function move_elves!(elves::Vector{Tuple{Int,Int}})
+
+        surround = falses(9)
+        NSWE = [[1, 4, 7], [3, 6, 9], [1, 2, 3], [7, 8, 9]]
+        NSWEstep = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+        start = 1
+        counter = 0
+        partone = 0
+
+        next = similar(elves)
+
+        while true
+
+                counter += 1
+                if counter == 11
+                        a = maximum(x[2] for x in elves) - minimum(x[2] for x in elves) + 1
+                        b = maximum(x[1] for x in elves) - minimum(x[1] for x in elves) + 1
+                        partone = a * b - length(elves)
+                end
+
+                tocheck = Set{Tuple{Int,Int}}(elves)
+
+                for (i, elf) in enumerate(elves)
+                        surround[1:end] .= false
+                        for (j, (x::Int, y::Int)) in enumerate((x, y) for y in -1:1, x in -1:1)
+                                x == y == 0 && continue
+                                (elf[1] + x, elf[2] + y) ∈ tocheck && (surround[j] = true)
+                        end
+
+                        if sum(surround) == 0
+                                next[i] = elf
+                                continue
+                        end
+                        canmove = false
+
+                        for o in start:start+3
+                                loccheck = NSWE[mod1(o, 4)]
+                                if !any(surround[loccheck])
+                                        canmove = true
+                                        next[i] = (elf .+ NSWEstep[mod1(o, 4)])
+                                        break
+                                end
+                        end
+
+                        if !canmove
+                                next[i] = elf
+                        end
+
+                end
+
+                done = true
+
+                nextcheck = countmap(next)
+                for (i, n) in enumerate(next)
+                        if nextcheck[n] == 1
+                                done && elves[i] != n && (done = false)
+                                elves[i] = n
+                        end
+                end
+                done && break
+                start += 1
+        end
+
+        partone, counter
+end
+
+
+function solutions(io::String=joinpath(@__DIR__, "..", "data", "23.txt"))
+        ispath(io) || (io = IOBuffer(io))
+        elves = make_elves(io)
+        partone, parttwo = move_elves!(elves)
+end
+
+end
