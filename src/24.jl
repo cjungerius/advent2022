a721467c2d644149cf6c499e6d93bd58a63a1810
Chris Jungerius <37599089+cjungerius@users.noreply.github.com>
2022-12-24 18:01:35 +0100

    solve p1 and p2

--- src/24.jl
+++ src/24.jl
@@ -1,39 +1,41 @@
 module Day24
 
+using DataStructures
+
 function f(io="data/24.txt")
 
     input = readlines(io)
-    valleyheight = length(input)-2
-    valleywidth = length(input[1])-2
+    valleyheight = length(input) - 2
+    valleywidth = length(input[1]) - 2
     timesteps = lcm(valleyheight, valleywidth)
-    accessible = trues(length(input),length(input[1]),timesteps)
+    accessible = trues(length(input), length(input[1]), timesteps)
     xs = Int[]
     ys = Int[]
 
 
-    for (y,line) in enumerate(input)
-        for (x,c) in enumerate(line)
-            if c=='#'
-                accessible[y,x,:].=false
-            elseif c=='>'
-                xs = 1 .+ mod1.(x-1 .+ [0:timesteps-1...], valleywidth)
-                for (t,i) in enumerate(xs)
-                    accessible[y,i,t] = false
+    for (y, line) in enumerate(input)
+        for (x, c) in enumerate(line)
+            if c == '#'
+                accessible[y, x, :] .= false
+            elseif c == '>'
+                xs = 1 .+ mod1.(x - 1 .+ [0:timesteps-1...], valleywidth)
+                for (t, i) in enumerate(xs)
+                    accessible[y, i, t] = false
                 end
-            elseif c=='<'
-                xs = 1 .+ mod1.(x-1 .- [0:timesteps-1...], valleywidth)
-                for (t,i) in enumerate(xs)
-                    accessible[y,i,t] = false
+            elseif c == '<'
+                xs = 1 .+ mod1.(x - 1 .- [0:timesteps-1...], valleywidth)
+                for (t, i) in enumerate(xs)
+                    accessible[y, i, t] = false
                 end
-            elseif c=='v'
-                ys = 1 .+ mod1.(y-1 .+ [0:timesteps-1...], valleyheight)
-                for (t,i) in enumerate(ys)
-                    accessible[i,x,t] = false
+            elseif c == 'v'
+                ys = 1 .+ mod1.(y - 1 .+ [0:timesteps-1...], valleyheight)
+                for (t, i) in enumerate(ys)
+                    accessible[i, x, t] = false
                 end
-            elseif c=='^'
-                ys = 1 .+ mod1.(y-1 .- [0:timesteps-1...], valleyheight)
-                for (t,i) in enumerate(ys)
-                    accessible[i,x,t] = false
+            elseif c == '^'
+                ys = 1 .+ mod1.(y - 1 .- [0:timesteps-1...], valleyheight)
+                for (t, i) in enumerate(ys)
+                    accessible[i, x, t] = false
                 end
             end
         end
@@ -41,9 +43,38 @@ function f(io="data/24.txt")
     accessible
 end
 
-function bfs(accessible)
-    start = [1,2]
-    finish = [size(accessible)[1],size(accessible)[2]-1]
+function bfs(accessible, start=(1, 2, 1), finish=(size(accessible)[1], size(accessible)[2] - 1))
+    distances = fill(-1, size(accessible))
+    distances[start...] = 0
+
+    q = Queue{Tuple{Int,Int,Int}}()
+    enqueue!(q, start)
+
+    neighbours = [(-1, 0), (1, 0), (0, 0), (0, 1), (0, -1)]
+
+    while !isempty(q)
+
+        y, x, t = dequeue!(q)
+        tnext = mod1(t + 1, size(accessible)[3])
+        y == finish[1] && break
+        for (dy, dx) in neighbours
+            y + dy == 0 && continue
+            y + dy > size(accessible)[1] && continue
+            if distances[y+dy, x+dx, tnext] < 0 && accessible[y+dy, x+dx, tnext]
+                distances[y+dy, x+dx, tnext] = distances[y, x, t] + 1
+                enqueue!(q, (y + dy, x + dx, tnext))
+            end
+        end
+    end
+    argmax(distances[finish..., :]), maximum(distances[finish..., :])
+end
+
+function back_and_forth(accessible)
+    fy, fx = size(accessible)[1], size(accessible)[2] - 1
+    phase_a, steps_a = bfs(accessible, (1, 2, 1), (fy, fx))
+    phase_b, steps_b = bfs(accessible, (fy, fx, phase_a), (1, 2))
+    phase_c, steps_c = bfs(accessible, (1, 2, phase_b), (fy, fx))
+    steps_a, +(steps_a, steps_b, steps_c)
 end
 
 end
